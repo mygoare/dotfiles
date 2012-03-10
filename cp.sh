9a49b7b6824492a6e3e21ca36b5ddd71e8a8bbe8
mygoare <mygoare@gmail.com>
2012-03-10 23:40:59 +0800

    cp.sh

--- cp.sh
+++ cp.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 OS=`uname`
 
-if ["$OS" == "Linux" || "$OS" == "Darwin"]
+if [ "$OS" = "Linux" ]
 then
 	cp ./.vimrc ../.vimrc
+elif [ "$OS" = "Darwin" ] 
+then
+	cp ./.vimrc ../_vimrc
 else
 	cp ./.vimrc ../_vimrc
 fi
+
