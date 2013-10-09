e4f8ab7664e42cd6b6312d9d5a88aab6ca561812
mygoare <mygoare@gmail.com>
2013-10-09 23:00:35 +0800

    vim +BundleInstall! +qall 改为手动

--- kit/install.sh
+++ kit/install.sh
@@ -23,8 +23,6 @@ else
   echo "\033[0;33mYou have installed vundle.\033[0m"
 fi
 
-/usr/bin/env vim -e +BundleInstall +qall
-
 for var in .gitconfig .jshintrc .tmux.conf .zshrc .zprofile; do
   [ -f $HOME/$var ] && mv $HOME/$var $HOME/.pre$var
   ln -sf ~/Projects/vim/dotfiles/$var $HOME/$var
