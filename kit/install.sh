076e049c408f48cb51b1a6e3ae55c4bdaa8cf31e
mygoare <mygoare@gmail.com>
2013-10-09 20:20:02 +0800

    rebuild

--- kit/install.sh
+++ kit/install.sh
@@ -1,16 +1,17 @@
+git --version 1>/dev/null 2>&1 || { echo "\033[0;31mgit not installed\033[0m"; exit 0; }
+zsh 1>/dev/null 2>&1 || { echo "\033[0;31mzsh not installed\033[0m"; exit 0; }
+
 if [ ! -d ~/Projects/vim/ ]; then
   mkdir -p ~/Projects/vim/
-else
-  echo "\033[0;33mYou have installed my vim config.\033[0m"
-  exit 0;
+  echo "\033[0;34mCloning Mygoare Vim...\033[0m"
+  git clone https://github.com/mygoare/.vim.git ~/Projects/vim/
+#else
+  #echo "\033[0;33mYou have installed my vim config.\033[0m"
+  #exit 0;
 fi
 
-git --version 1>/dev/null 2>&1 || { echo "\033[0;31mgit not installed\033[0m"; exit 0; }
-
-echo "\033[0;34mCloning Mygoare Vim...\033[0m"
-git clone https://github.com/mygoare/.vim.git ~/Projects/vim/
 if [ -f ~/.vimrc ]; then
-  mv ~/.vimrc ~/.pre-vimrc
+  mv ~/.vimrc ~/.pre.vimrc
 fi
 ln -sf ~/Projects/vim/.vimrc ~/.vimrc
 
@@ -27,8 +28,10 @@ vim -e +BundleInstall! +qall
 echo "\033[0;34mChange shell to zsh...\033[0m"
 chsh -s `which zsh`
 
-echo "\033[0;34mCloning Oh-My-Zsh...\033[0m"
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "\033[0;34mCloning Oh-My-Zsh...\033[0m"
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 /usr/bin/env zsh
 source ~/.zshrc
 
