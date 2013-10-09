a91e439bbbad58d817872775e1091475fcb0098c
mygoare <mygoare@gmail.com>
2013-10-09 18:11:53 +0800

    update

--- kit/install.sh
+++ kit/install.sh
@@ -1,11 +1,11 @@
 if [ ! -d ~/Projects/vim/ ]; then
   mkdir -p ~/Projects/vim/
 else
-  echo "\033[0;33]You have installed my vim config.\033[0m]"
+  echo "\033[0;33mYou have installed my vim config.\033[0m"
   exit 0;
 fi
 
-git --version 1>/dev/null 2>&1 || { echo "\033[0;31]git not installed"; exit 0\033[0m]; }
+git --version 1>/dev/null 2>&1 || { echo "\033[0;31mgit not installed"; exit 0\033[0m; }
 
 git clone https://github.com/mygoare/.vim.git ~/Projects/vim/
 if [ -f ~/.vimrc ]; then
@@ -16,11 +16,7 @@ ln -sf ~/Projects/vim/.vimrc ~/.vimrc
 if [ ! -d ~/.vim/bundle/vundle/ ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 else
-  echo "
-    \033[0;33]
-    \nYou have installed vundle\n
-    \033[0m]
-    "
+  echo "\033[0;33mYou have installed vundle.\033[0m"
 fi
 vim +BundleInstall! +qall
 
