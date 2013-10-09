e41b6e91c61edb6cc17209d9b43aeb6b306f6e9d
mygoare <mygoare@gmail.com>
2013-10-09 17:27:33 +0800

    up

--- kit/install.sh
+++ kit/install.sh
@@ -4,7 +4,7 @@ fi
 
 git --version 1>/dev/null 2>&1 || { echo "git not installed"; exit 0; }
 
-git clone git@github.com:mygoare/.vim.git ~/Projects/vim/
+git clone https://github.com/mygoare/.vim.git ~/Projects/vim/
 if [ -f ~/.vimrc ]; then
   mv ~/.vimrc ~/.pre.vimrc
 fi
