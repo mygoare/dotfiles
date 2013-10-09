ca964a3f2a01972e05fddd525501551a260e416d
mygoare <mygoare@gmail.com>
2013-10-09 19:34:48 +0800

    增加提醒， 增加oh-my-zsh

--- kit/install.sh
+++ kit/install.sh
@@ -7,6 +7,7 @@ fi
 
 git --version 1>/dev/null 2>&1 || { echo "\033[0;31mgit not installed\033[0m"; exit 0; }
 
+echo "\033[0;34mCloning Mygoare Vim...\033[0m"
 git clone https://github.com/mygoare/.vim.git ~/Projects/vim/
 if [ -f ~/.vimrc ]; then
   mv ~/.vimrc ~/.pre-vimrc
@@ -14,12 +15,17 @@ fi
 ln -sf ~/Projects/vim/.vimrc ~/.vimrc
 
 if [ ! -d ~/.vim/bundle/vundle/ ]; then
+  echo "\033[0;34mCloning Vundle...\033[0m"
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 else
   echo "\033[0;33mYou have installed vundle.\033[0m"
 fi
 
 vim -e +BundleInstall! +qall
+
+#add oh-my-zsh
+echo "\033[0;34mCloning Oh-My-Zsh...\033[0m"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 for var in .gitconfig .jshintrc .tmux.conf .zshrc .zprofile; do
   [ -f $HOME/$var ] && mv $HOME/$var $HOME/.pre$var
