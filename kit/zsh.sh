59d809415cd6318c33d563f695d5a9b2bb22d4a4
Goare Mao <mygoare@gmail.com>
2013-10-10 08:10:49 +0800

    Update zsh.sh
    change shell order to make it work

--- kit/zsh.sh
+++ kit/zsh.sh
@@ -1,11 +1,12 @@
-zsh 1>/dev/null 2>&1 || { echo "\033[0;31mzsh not installed\033[0m"; exit 0; }
-
-echo "\033[0;34mChange shell to zsh...\033[0m"
-sudo chsh -s `which zsh`
+zsh --version 1>/dev/null 2>&1 || { echo "\033[0;31mzsh not installed\033[0m"; exit 0; }
 
 if [ ! -d ~/.oh-my-zsh ]; then
   echo "\033[0;34mCloning Oh-My-Zsh...\033[0m"
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
+
+echo "\033[0;34mChange shell to zsh...\033[0m"
+chsh -s `which zsh`
+
 /usr/bin/env zsh
 source ~/.zshrc
